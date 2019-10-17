8aebf4b5c0a4e1a2f89f0ea9bd57b2352f1b4859
Celeste Jimenez <celestejimenez@Celestes-MacBook-Air.local>
2019-10-16 23:40:19 -0700

    1

---
+++ app.js
@@ -0,0 +1,42 @@
+
+    /*
+attempted my code really late and didn't establish a proper foundation.
+understand the concept but didn't give myself enough time to figure out the syntax,
+or the proper functions.
+
+balloon.style.setProperty('--element-font-size',font-size + 'px')
+
+var font = font-size;
+
+document.addEventListener('keyup',handler => {
+    while (font>=0 && font<=60) {
+        if (event.keyCode='I') {
+            font+=10;
+            alert('works');
+        } elseif (event.keyCode='D'); {
+            font-=10;
+            alert('also works');
+        }    
+    }
+}
+);
+
+if (font<0) {
+    document.removeEventListener('keyup', handler);
+    document.write('Done');
+    alert('this works');
+} elseif (font>60); {
+    document.removeEventListener('keyup', handler);
+    document.write (`&#128165`);
+    alert('this also works');
+}
+
+const handler = function (balloon) {
+    while (font>=0 && font<=60) {
+    font-= 10;
+    }
+
+}
+
+*/
+
